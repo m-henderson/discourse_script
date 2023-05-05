837c7656472ece7c2831df3b45468f3e353fbedc
Michael <michael.v.henderson@icloud.com>
2023-05-05 14:56:42 -0500

    ad chatgpt instead of writeful

--- writefull.py
+++ writefull.py
@@ -1,14 +1,20 @@
-from requests import post
+
+import openai
+openai.api_key = "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"
+
 import json
 # what if we get an error on writefull api? how do we check status codes
-def generate_title(title):
-    arr = []
-    arr.append(title)
-    data = json.dumps(arr)
-    # needs to be in array ["atest"]
-    test = post("https://nlp.writefull.ai/title-generator", data=data, headers={"Content-Type":"application/json", "Accept": "*/*"})
-    if test.status_code == 200:
-        v = test.json()
-        return v["result"]
-    else:
-        return title[:50]
+def generate_title(discourse_post_body):
+    response = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo" ,
+    max_tokens = 280,
+    messages = [
+            {"role": "system", "content": "Generate a title no longer than 40 characters to the text I am about to give you."},
+            {"role": "user","content": discourse_post_body}
+    ])
+    
+    message = response.choices[0]['message']
+    return message
+
+
+
